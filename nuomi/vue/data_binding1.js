f66ad12651d4cb4533de14f322f08d42f267cfd3
Runner <841766635@qq.com>
2017-03-02 11:14:28 +0800

    任务一。setter,getter.

---
+++ nuomi/vue/data_binding1.js
@@ -0,0 +1,60 @@
+function Observer(data) {
+	this.data = data;
+	this.walk(data);
+}
+
+Observer.prototype = {
+	construtor: Observer,
+	walk: function(data) {
+		var key, val;
+		for (key in data) {
+			if (!data.hasOwnProperty(key)) continue;
+			val = data[key];
+			//如果是属性是对象的话，递归调用
+			if (Object.prototype.toString.call(val) === '[object Object]') {
+				new Observer(val);			
+			}
+			this.defineData(key, val);
+		}
+	},
+	defineData: function(key, val) {		
+		//其实这里是用了一个闭包来保存了key和val。
+		//每次调用这个函数就会重新定义原来的属性。原来的val值要从这个闭包中读取。		
+		Object.defineProperty(this.data, key, {
+			enumerable: true,
+			configurable: true,
+			get: function() {
+				console.log('你访问了' + key);
+				return val;
+			},
+			set: function(newVal) {
+				console.log('你设置了' + key + '，新的值为' + newVal);
+				if (val === newVal) return;
+				val = newVal;
+			}
+		})
+	}
+};
+
+//测试
+var data = {
+	a:1,
+	b:2,
+	c:{
+		aa:11,
+		bb:22,
+		cc:{
+			aaa:111,
+			bbb:222,
+		}
+	}
+}
+//测试
+// new Observer(data);
+// data.c.cc.aaa;
+// data.c.cc.aaa = 111.1
+// data.a;
+// data.a = 1.1;
+//删除是不会检测到的。
+// delete data.a;
+// data.a;
