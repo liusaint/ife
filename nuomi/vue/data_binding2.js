b7bb4cd8adfd677b80b93185b012fba864f12b75
Runner <841766635@qq.com>
2017-03-02 14:19:11 +0800

    增加个简单的事件处理器

--- nuomi/vue/data_binding2.js
+++ nuomi/vue/data_binding2.js
@@ -22,7 +22,8 @@ Observer.prototype = {
 			this.defineData(key, val);
 		}
 	},
-	defineData: function(key, val) {		
+	defineData: function(key, val) {
+
 		//其实这里是用了一个闭包来保存了key和val。
 		//每次调用这个函数就会重新定义原来的属性。原来的val值要从这个闭包中读取。		
 		Object.defineProperty(this.data, key, {
@@ -44,11 +45,63 @@ Observer.prototype = {
 			}
 		})
 	},
+	//绑定事件，观测者模式
 	$watch:function(path,callBack){
+
+
+	},
+	runCallBacks:function(path){
 
 	}
 };
 
+
+//用于事件处理
+function Event(){
+	this.eventObj = {};
+}
+Event.prototype = {
+	construtor:Event,
+	//绑定事件
+	on:function(key,callBack){
+		if(!this.eventObj[key]){
+			this.eventObj[key] = [];
+		}
+		this.eventObj[key].push(callBack);
+	},
+	//解绑事件
+	off:function(key){
+		if(this.eventObj[key]){
+			delete this.eventObj[key];
+		}
+	},
+	// 触发事件
+	emit:function(key){
+		var callBackArr = this.eventObj[key]||[];
+		var slice = Array.prototype.slice;
+		var args = slice.call(arguments,1)
+		var len = callBackArr.length;
+		var i = 0;
+		for (; i < len; i++) {
+			typeof callBackArr[i] === 'function' && callBackArr[i].apply('',args);
+		}
+	}
+}
+
+var event = new Event();
+event.on('aa',function(a) {	
+	console.log('aa1'+a);
+});
+event.on('bb',function(b) {	
+	console.log('bb1'+b);
+});
+event.on('aa',function(a) {	
+	console.log('aa2'+a);
+});
+
+event.emit('aa','test1');
+event.off('aa');
+event.emit('aa','test1');
 //测试
 var data = {
 	a:1,
@@ -64,20 +117,11 @@ var data = {
 }
 //测试
 // new Observer(data);
-// data.c.cc.aaa;
-// data.c.cc.aaa = 111.1
-// data.a;
-// data.a = 1.1;
-//删除是不会检测到的。
-// delete data.a;
-// data.a;
+// data.c;
+// data.c = {
+// 	a:{
+// 		b:1
+// 	}
+// }
 
-new Observer(data);
-data.c;
-data.c = {
-	a:{
-		b:1
-	}
-}
-
-data.c.a.b
+// data.c.a.b
